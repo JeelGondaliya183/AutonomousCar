7037e8ae9237bfc9b686a9470d3283efe95f1796
JeelGondaliya183 <jeelgondaliya1997@gmail.com>
2020-11-22 11:20:24 -0400

    changes

--- AutonomousCar/dataCollection.py
+++ AutonomousCar/dataCollection.py
@@ -4,7 +4,8 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture('vid.mp4')
     while True:
         _, img = cap.read() # GET THE IMAGE
-        img = cv2.resize(img,(640,480)) # RESIZE
+        #img = cv2.resize(img,(640,480)) # RESIZE
         cv2.show(img)
-        #getLaneCurve(img)
         cv2.waitKey(1)
+
+
